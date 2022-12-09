fd3b644f7f20877da3fcbe31c0698e66b32d8d50
Ypd20 <101164198+Ypd20@users.noreply.github.com>
2022-12-09 10:01:38 +0530

    Delete Set exercises directory

--- Set exercises/set_ex_1.py
+++
@@ -1,4 +0,0 @@
-sample_set = {"Yellow", "Orange", "Black"}
-sample_list = ["Blue", "Green", "Red"]
-sample_set.update(sample_list)
-print(sample_set)
--- Set exercises/set_ex_5.py
+++
@@ -1,3 +0,0 @@
-set1 = {10, 20, 30, 40, 50}
-set1.difference_update({10,20,30})
-print(set1)
