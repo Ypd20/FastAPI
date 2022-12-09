a8d0f43af2b9bb2e92ba2b27cd90391722da1ef3
Ypd20 <101164198+Ypd20@users.noreply.github.com>
2022-12-09 10:01:12 +0530

    Delete OOPs exercises directory

--- OOPs exercises/oop_ex_1.py
+++
@@ -1,8 +0,0 @@
-class Vehicle:
-    def __init__(self,max_speed,mileage):
-        self.max_speed=max_speed
-        self.mileage=mileage
-car1=Vehicle(120,25)
-print(car1.max_speed,car1.mileage)
-
-        
