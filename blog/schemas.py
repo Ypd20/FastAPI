f8b2c5e858f17205f8d44213478679b605de8750
Ypd20 <desaiyashp20@gmail.com>
2022-12-10 18:41:39 +0530

    FastAPI

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,11 +1,16 @@
+from typing import List 
 from pydantic import BaseModel
-class Blog(BaseModel):
+
+class BlogBase(BaseModel):
     title: str
     body: str
 
-class ShowBlog(BaseModel):
-    title : str
-    body : str
+class Blog(BlogBase):
+
+    class Config():
+        orm_mode = True
+
+
     class Config():
         orm_mode = True
 
@@ -13,3 +18,24 @@ class User(BaseModel):
     name : str
     email : str
     password : str
+
+class ShowUser(BaseModel):
+    name : str
+    email : str
+    blogs : List[Blog] = []
+
+    class Config():
+        orm_mode = True
+
+class ShowBlog(BaseModel):
+    title : str
+    body : str
+    creator : ShowUser
+
+    class Config():
+        orm_mode = True
+
+
+
+
+
